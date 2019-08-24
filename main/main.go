baa9b1f48c06ea7b71cacb0995f5b783d73284e4
evanlesmez <evan.lesmez@gmail.com>
2019-08-24 16:46:39 -0400

    todo notes

---
+++ main/main.go
@@ -0,0 +1,13 @@
+// TODO hookup sqlite3 database with curated animations & music
+// TODO display animations & play music on each page (lofi with my fav animations to start)
+// TODO add submit an animation to my database or song to me for review to put in queue
+// TODO give me email if you want to submit a new one
+
+// TODO host this on wifi, storage microelectronic (my raspberry pi probably once new one comes in)
+
+// TODO fetch new animations & music in backend based on genre tags
+// TODO filter animations & music based on mood
+
+// TODO Donate to me with cash
+
+// TODO use bitcoin to vote on next song out of 3
